09d4f2c3bc38a3554ba24d07315b7fcb3d679f4a
Timon <votolggogogo@gmail.com>
2023-08-28 21:05:35 +0300

    'initial'

---
+++ strcounter.py
@@ -0,0 +1,25 @@
+# На вход подаётся строка. Нужно посчитать и вывести в консоль каждого из символов этой строки
+# Реализовать ввиде функции 
+# Вход: aabccd
+# Выход: a - 2, b - 1, c -2, d - 1
+
+def strcounter(s):
+    print(set(s))
+    for sym in s:
+        counter = 0
+        for sub_s in s:
+            if sym == sub_s:
+                counter += 1 # c += 1    c = c+1
+        print(sym, '-',counter)
+
+#strcounter('abcd')
+
+def strcounter2(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    for sym, count in syms_counter.items():
+        print(sym, '-', count)
+
+strcounter2('aabbbcccccccde')
